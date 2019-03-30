a04797ae3abb99fb8b69cb2e15efc06bb8d53b6c
Tylopilus <elitaer@gmail.com>
2019-03-30 13:28:13 +0100

    added navbar and restructured

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
 
-app.get('/api/customers', (req, res) => {
-    const customers = [
-        {id: 1, firstName: "John", lastName: "FickDichDoch"},
-        {id: 2, firstName: "Mary", lastName: "Didadoe"},
-        {id: 3, firstName: "Peter", lastName: "Doe"}
-    ]
+app.use(cors())
+app.use(express.json())
 
-    res.json(customers)
+const customers = [
+    {id: 1, name: "App", url: "/app", edit: false},
+    {id: 2, name: "Info", url: "/info", edit: false},
+    {id: 3, name: "About", url: "/about", edit: false}
+]
+app.get('/api/urls', (req, res) => {
+    res.json({data: customers})
 })
+
+app.get('/api/urls/:id', (req, res) => {
+    return res.send(customers[req.params.id-1])
+})
+
+app.put('/api/urls/:id', (req, res) => {
+    customers.forEach(r => {
+        if(r.id === parseInt(req.params.id)){
+            //console.log(r.id, req.body.data)
+            r.name = req.body.data
+        }
+    })
+})
+
+app.post('/api/urls/:id', (req, res) => {
+    console.log(req.params.id)
+    return res.send(req.params.id)
+})
+
 
 const port = 5000;
 
