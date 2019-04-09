55b08a5ced1f008a82c632894767dec819428ed7
Tylopilus <elitaer@gmail.com>
2019-04-09 11:10:08 +0200

    returned error message

--- server/Modules/Controllers/TournamentController.js
+++ server/Modules/Controllers/TournamentController.js
@@ -7,9 +7,10 @@ async function postTournament (req, res) {
         res.send(
             tournament.toJSON(),
         )
-    } catch {
+    } catch (err){
         res.status(400).send({
-            error: 'Could not create Tournament'
+            error: 'Could not create Tournament',
+            msg: err
         })
     }
 }
